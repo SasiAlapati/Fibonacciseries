3a419a920a47637d363b810e7a49a799c4f4c4bc
sasi <sasi.alapati@orangescape.com>
2016-03-07 11:42:23 +0530

    my first commit

---
+++ fib.py
@@ -0,0 +1,12 @@
+def fib(n):
+	if n<=1:
+		return n
+	else:
+		return fib(n-1)+fib(n-2)
+try:
+	n=int(input("enter num"))
+except ValueError:
+	print(value is not positive)
+if(n>=1):
+	for i in range(n):
+		print (fib(i))
