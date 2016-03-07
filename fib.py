f4b2ab6e6d89e8b68713555186a0ca85ed651803
sasi <sasi.alapati@orangescape.com>
2016-03-07 11:50:26 +0530

    terms number modified from 10 to 20

--- fib.py
+++ fib.py
@@ -3,10 +3,10 @@ def fib(n):
 		return n
 	else:
 		return fib(n-1)+fib(n-2)
-try:
-	n=int(input("enter num"))
-except ValueError:
-	print(value is not positive)
-if(n>=1):
-	for i in range(n):
+#try:
+#	n=int(input("enter num"))
+#except ValueError:
+#	print(value is not positive)
+#if(n>=1):
+	for i in range(20):
 		print (fib(i))
